3575a72d6a68324011681ec299a907f44c741eec
Amber Thomas <amber.thomas@data.world>
2022-03-02 11:54:01 -0800

    adding missing links and checks for downloads

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const { chromium } = require('playwright');
 const fs = require('fs');
+const needle = require('needle')
+require('dotenv').config()
 
 
 let page;
 
-const words = ['pric', 'insurance', 'cms', 'estimat', 'charg']
-const fileTypes = ['xls', 'csv', 'json', 'ashx', 'txt']
+const words = ['pric', 'insurance', 'cms', 'estimat', 'charg', 'pay']
+const fileTypes = ['xls', 'csv', 'json', 'ashx', 'txt', 'zip']
 
 let allFileUrls = [];
 let allCheckedUrls = [];
 let data = [];
+let missingData = [];
 
 // list of test hospitalURLs
-const hospitalURLs = [ 'https://www.providence.org/', 'http://www.christushealth.org', 'http://oregon.providence.org/location-directory/p/providence-st-vincent-medical-center/', 
-'https://www.mayoclinic.org/']
+const hospitalURLs = [ 'https://www.nyp.org/morganstanley', 'https://www.hopkinsmedicine.org/', 'https://www.providence.org/', 'http://www.christushealth.org', 'http://oregon.providence.org/location-directory/p/providence-st-vincent-medical-center/', 
+'https://www.mayoclinic.org/', 'http://www.partners.org/', 'http://www.massgeneral.org/international', 'http://www.unitypoint.org/', 'https://www.nyp.org/morganstanley']
 
 
 async function getDomain(url){
@@ -52,7 +55,7 @@ async function checkPageForText(text){
         return links.map(link => {
             return link.href
         })
-    })
+    }).catch(error => console.error({error}))
 
     // return unique URLs with a text match for "text" string
     return [...new Set(linkUrls)]
@@ -62,7 +65,7 @@ async function checkPageForText(text){
 async function checkForAllFiles(){
     let allFoundFiles = [];
 
-    for (type of fileTypes) {
+    for await (type of fileTypes) {
         // find all link elements with file type
         const aElements = page.locator(`a[href*=${type}]`)
         
@@ -89,7 +92,7 @@ async function checkForAllFiles(){
 async function checkForAllWords(){
     let allFoundWords = [];
 
-    for (const word of words){
+    for await (const word of words){
         const foundWords = await checkPageForText(word)
 
         if (foundWords.length) allFoundWords.push(foundWords)
@@ -101,46 +104,66 @@ async function checkForAllWords(){
 }
 
 async function checkUrl(url){
+    console.log({url})
 
     try {
 
         // check if we've already looked at this url
         const alreadyChecked = allCheckedUrls.includes(url)
 
+        // make sure it isn't a mailto url
+        const mail = url.includes('mailto')
+
         // if it hasn't already been checked
-        if (alreadyChecked === false) {
+        if (!alreadyChecked && !mail) {
 
             // add url to list of urls that have been checked
             allCheckedUrls.push(url)
 
-            // go to page
-            await page.goto(url, {waitUntil: 'domcontentloaded'})
+            if (url.includes('download')){
+                // if the url triggered a download, add that download to allFiles and don't go to page
+                const [download] = await Promise.all([
+                    page.waitForEvent('download'),
+                    page.locator(`a[href="${url}"]`).click()
+                ])
+                download.delete()
+                const downloadUrl = download.url()
+                allFileUrls.push({foundAt: url, files: [downloadUrl]})
+                console.log({allFileUrls})
 
-            // checks page for word matches in a text or href
-            const foundWords = await checkForAllWords();
 
-            // check page for files of the type we're looking for
-            const foundFiles = await checkForAllFiles();
+            } else {
+                // if the url doesn't appear to trigger a download, navigate to it
+                // go to page
+                await page.goto(url, {waitUntil: 'domcontentloaded'})
 
-            //console.log({url, foundWords, foundFiles})
+                // checks page for word matches in a text or href
+                const foundWords = await checkForAllWords();
 
-            if (foundWords.length && !foundFiles.length && !allFileUrls.length){
-                // if word match links were found but no files, run it again
-                for (const foundWordUrl of foundWords){
-                    if (foundWordUrl != url) await checkUrl(foundWordUrl);
+                // check page for files of the type we're looking for
+                const foundFiles = await checkForAllFiles();
+
+                //console.log({url, foundWords, foundFiles})
+
+                if (foundWords.length && !foundFiles.length && !allFileUrls.length){
+                    // if word match links were found but no files, run it again
+                    for await (const foundWordUrl of foundWords){
+                        if (foundWordUrl != url) await checkUrl(foundWordUrl);
+                    }
                 }
+
+                else if (foundFiles.length){
+                    const flatFileFindings = foundFiles.flat().flat();
+
+                    const uniqueFindings = [...new Set(flatFileFindings)]
+
+                    // if files were found, add them to our empty array
+                    allFileUrls.push({foundAt: url, files: flatFileFindings})
+                }
+
             }
 
-            else if (foundFiles.length){
-                const flatFileFindings = foundFiles.flat().flat();
-
-                const uniqueFindings = [...new Set(flatFileFindings)]
-
-                // if files were found, add them to our empty array
-                allFileUrls.push({foundAt: url, files: flatFileFindings})
-            }
         }
-
 
         return 'success'
     }
@@ -161,10 +184,11 @@ async function checkForDuplicates(a, b){
 async function combineData(url){
     let fileUrls = allFileUrls.map(d => {
         return {
-            ...d,
-            originalUrl: url
+            originalUrl: url,
+            foundAt: [d.foundAt],
+            files: d.files
         }
-    })
+    })[0]
 
     if (allFileUrls.length > 1) {
         const dupes = checkForDuplicates(allFileUrls[0].files, allFileUrls[1].files)
@@ -177,8 +201,8 @@ async function combineData(url){
 
 }
 
-async function writeData(str){
-    fs.writeFile('data/links.json', str, err => {
+async function writeLocally(str, path){
+    fs.writeFile(path, str, err => {
         if (err) {
             console.error(err)
             return
@@ -186,24 +210,37 @@ async function writeData(str){
     })
 }
 
-async function searchPage(){
-    // type price transparency into search bar
-    await page.fill('input:has-text(`Search`)', 'price transparency')
-    await page.press('input:has-text(`Search`)', 'Enter')
+async function writeToDDW(path, filename){
+    const agentid = 'amberthomas'
+    const datasetid = 'location-of-price-transparency-data'
 
-    // checks page for word matches in a text or href
-    const foundWords = await checkForAllWords();
-
-    for (const url of foundWords){
-        await checkUrl(url)
+    const fileData = {
+        file: {
+            file: path,
+            content_type: 'application/json',
+            filename
+        }
+    }
+    
+    const fileOptions = {
+        headers: {
+            'authorization': `Bearer ${process.env.DDW_TOKEN}`
+        },
+        multipart: true
     }
 
+    needle('post', 
+    `https://api.data.world/v0/uploads/${agentid}/${datasetid}/files`, fileData, fileOptions)
+        .then(resp => console.log(resp.body, resp.statusCode, resp.headers))
 
 }
 
+async function writeData(str, filename, path){
 
+    await writeLocally(str, path)
+    await writeToDDW(path, filename)
 
-
+}
 
 
 
@@ -219,7 +256,7 @@ async function searchPage(){
     // launch a new page and save to global variable page
     page = await context.newPage();
 
-    for (const [index, url] of hospitalURLs.entries()){
+    for await (const [index, url] of hospitalURLs.entries()){
         
         // reset to blank
         allFileUrls = [];
@@ -231,34 +268,33 @@ async function searchPage(){
 
             const textFindings = await checkUrl(url)
 
-            if (textFindings === 'success' && !allFileUrls.length){
-                // last ditch effort if no files have been found so far; 
-                // find a search bar of some kind, type in "price transparency, enter, and run the process again on the results"
-                const searchFindings = await searchPage()
+
+            if (textFindings === 'success' && allFileUrls.length){
+
+                await combineData(url)
+                console.log(`Found ${index}/${hospitalURLs.length - 1}`)
+
+            } else {
+                // if the script has finished looking but found nothing
+                // add the url to our list of sites with missing information
+                missingData.push({originalUrl: url})
+                console.log(`Missing ${index}/${hospitalURLs.length - 1}`)
             }
 
-            // if (textFindings === 'success' && allFileUrls.length){
+            if (index === hospitalURLs.length - 1) {
+                console.log('last one!')
+                await context.close()
+                await browser.close()
+                const allStr = JSON.stringify(data)
+                await writeData(allStr, 'links.json', 'data/links.json');
 
-            //     await combineData(url)
-
-            // } else {
-            //     // last ditch effort, find a search bar of some kind, type in "price transparency, enter, and run the process again on the results"
-            // }
-
-            // if (index === hospitalURLs.length - 1) {
-            //     console.log('last one!')
-            //     await context.close()
-            //     await browser.close()
-            //     const allStr = JSON.stringify(data)
-            //     await writeData(allStr);
+                const missingStr = JSON.stringify(missingData)
+                await writeData(missingStr, 'missingLinks.json', 'data/missingLinks.json')
     
-            // }
+            }
     
-
         }
 
-
-            
 
 
         // TODO
